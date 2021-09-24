f169b6ed42195ef3a7418bf4331cbaa5b9bccad6
GuardNexus <72446184+GuardNexusGN@users.noreply.github.com>
2021-09-24 14:41:13 +0300

    added ansible user

--- winrm.ps1
+++ winrm.ps1
@@ -399,3 +399,21 @@ Else
 Write-VerboseLog "PS Remoting has been successfully configured for Ansible."
 
 "Done"
+
+################
+# Ansible user #
+################
+$length = 12
+$nonAlphaChars = 6
+Add-Type –AssemblyName 'System.Web'
+$user = "ansible"
+$pass = ([System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars))
+$secureString = ConvertTo-SecureString $pass –AsPlainText –Force
+New-LocalUser –Name $user –Password $secureString
+$credential = New-Object System.Management.Automation.PsCredential($user,$secureString)
+$process = Start-Process cmd /c –Credential $credential –ErrorAction SilentlyContinue –LoadUserProfile
+$newPass = ([System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars))
+$newSecureString = ConvertTo-SecureString $newPass –AsPlainText –Force
+Set-LocalUser –Name $user –Password $newSecureString
+New-Item –Path "C:\Users\$user" –Name ".ssh" –ItemType Directory
+Add-LocalGroupMember -Group "Administrators" -Member $user
