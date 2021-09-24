a98c35668b0a052525a3d3787ff4541d13e9043a
GuardNexus <72446184+GuardNexusGN@users.noreply.github.com>
2021-09-24 14:43:07 +0300

    deleted useless text

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-#  (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/GuardNexusGN/Ansible-on-Windows-OpenSHH/main/ssh_ansible.ps1','ssh_ansible.ps1'); ./ssh_ansible.ps1; echo 'KEY' > C:/Users/ansible/.ssh/authorized_keys
-
 #  (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/GuardNexusGN/Ansible-on-Windows-OpenSHH/main/test.ps1','test.ps1'); ./test.ps1
 
 echo Done
+
+
