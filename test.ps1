dd59e0479168cbbf81adeb5fbaf632bd21f8565e
GuardNexus <72446184+GuardNexusGN@users.noreply.github.com>
2021-09-24 14:17:02 +0300

    updated command

--- test.ps1
+++ test.ps1
@@ -1,2 +1,2 @@
-#  (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/GuardNexusGN/Ansible-on-Windows---OpenSHH/main/test.ps1','test.ps1'); ./test.ps1
-echo test
+#  (new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/GuardNexusGN/Ansible-on-Windows-OpenSHH/main/test.ps1','test.ps1'); ./test.ps1
+echo Done
